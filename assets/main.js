55e24f2278818dcb51957380b8fd183a6f9c2de2
Omer Faruq <omerfaruq4@gmail.com>
2025-02-06 14:43:29 +0600

    faq done

--- assets/main.js
+++ assets/main.js
@@ -50,3 +50,25 @@ function prevSlide() {
 
 nextBtn.addEventListener("click", nextSlide);
 prevBtn.addEventListener("click", prevSlide);
+
+document.querySelectorAll(".faq-question").forEach((item) => {
+  item.addEventListener("click", () => {
+    const answer = item.nextElementSibling;
+    const icon = item.querySelector(".faq-icon");
+
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+      icon.textContent = "+";
+    } else {
+      document
+        .querySelectorAll(".faq-answer")
+        .forEach((ans) => (ans.style.display = "none"));
+      document
+        .querySelectorAll(".faq-icon")
+        .forEach((icn) => (icn.textContent = "+"));
+
+      answer.style.display = "block";
+      icon.textContent = "-";
+    }
+  });
+});
